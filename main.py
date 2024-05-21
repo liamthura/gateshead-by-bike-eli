c24582620add15dadea380d1398fd11c6f280beb
khantthura <42053354+khantthura@users.noreply.github.com>
2024-05-21 06:50:57 +0100

    base file commit

--- main.py
+++ main.py
@@ -1,41 +1,93 @@
-# This is a sample Python script.
+import sqlalchemy as sa
+from pywebio import *
 from pywebio.input import *
 from pywebio.output import *
+from sqlalchemy import Integer, ForeignKey
+from sqlalchemy.exc import SQLAlchemyError
+from sqlalchemy.orm import Mapped, mapped_column, sessionmaker, declarative_base, relationship
 
-put_markdown("# Covid Symptoms Checker #")
+# Creating a SQL Database 'gbb-eli.db' with SQLAlchemy
+sqlite_file_name = "gbb-eli.db"
+sqlite_url = f"sqlite:///{sqlite_file_name}"
+db = sa.create_engine(sqlite_url, echo=True)
+Session = sessionmaker(bind=db)
+Base = declarative_base()
 
-symptoms = ['Fever', 'Cough', 'Headache', 'Sore Throat', 'New loss of taste or smell']
 
-controls = [
-    checkbox("Symptoms", options=symptoms, name="symptoms"),
-    slider("Temperature", value=37, min_value=30, max_value=45, name="temp"),
-    actions('Is this Covid?', ['Check for Covid', ], name="action")
-]
+# Defining the User class with table name 'users'
+class User(Base):
+    __tablename__ = 'users'
 
-result = input_group("Symptoms", controls)
-temp = result['temp']
-cough = 'Cough' in (result['symptoms'])
-smellLoss = 'New loss of taste or smell' in (result['symptoms'])
-headache = 'Headache' in (result['symptoms'])
-soreThroat = 'Sore Throat' in (result['symptoms'])
-fever = 'Fever' in (result['symptoms'])
+    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+    username: Mapped[str] = mapped_column(unique=True)
+    display_name: Mapped[str]
+    password: Mapped[str]
+    role_id: Mapped[int] = mapped_column(ForeignKey("roles.id"))
+    subscription_status: Mapped[bool] = mapped_column(default=False)
+    # posts: Mapped[list["ParkingPost"]] = relationship("Post", back_populates="user_id")
 
-print(f'{temp=}, {cough=}, {smellLoss=} {headache=}, {soreThroat=}, {fever=}')
+    # Structuring output of User object for better readability
+    def __repr__(self):
+        return f"<User(id={self.id}, username={self.username})>"
 
-if temp < 32:
-    toast(f'Looks like you might be dead', 6)
-    message = """You might be a vampire or your thermometer is broken."""
-elif temp > 38 and (cough or smellLoss or headache or soreThroat or fever):
-    toast(f'Looks like you have have Covid', 6)
-    message = """
-        Take a Covid-19 test. Stay home, isolate yourself. Notify close contacts.
-        """
-else:
-    message = """You're fine get back to work!"""
 
-put_markdown(message)
+class Role(Base):
+    __tablename__ = 'roles'
 
-# I made changes to this file, I hope it's okay!
-# yeah of course it's fine
+    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+    name: Mapped[str] = mapped_column(unique=True)
+    users: Mapped[list["User"]] = relationship("User", back_populates="role_id")
 
-#Hello ivyyy
+    # Structuring output of Role object for better readability
+    def __repr__(self):
+        return f"<Role(id={self.id}, name={self.name})>"
+
+
+# Defining the Post class with table name 'posts'
+# class ParkingPost(Base):
+#     pass
+#
+#
+# class ParkingRating(Base):
+#     pass
+#
+#
+# class Thread(Base):
+#     pass
+#
+#
+# class ContentReport(Base):
+#     pass
+#
+#
+# class CrimeReport(Base):
+#     pass
+#
+#
+# class PoliceNotification(Base):
+#     pass
+#
+#
+# class SiteNotification(Base):
+#     pass
+
+# Base.metadata.create_all(db)
+
+@use_scope('ROOT')
+def main():
+    clear()
+    put_html(f'''
+    <h1>
+    Welcome to GBB-ELI
+    <small class="text-body-secondary">A parking lot management system for Gateshead Bike Users</small>
+    </h1>
+    <h4>
+    Khant Thura
+    <small class="text-body-secondary">2022-03-12</small>
+    </h4>
+    ''')
+    # put_buttons(['Login', 'Register'], onclick=[login, register])
+
+
+if __name__ == '__main__':
+    start_server(main, port=8080, host='localhost', debug=True)
