c4cc5429b6f67be73e5a5c1d9ad61970914aff2b
khantthura <42053354+khantthura@users.noreply.github.com>
2024-05-27 06:42:42 +0100

    changes

--- main.py
+++ main.py
@@ -1049,7 +1049,8 @@ def get_threads(user_id=None):
                     put_column([threadBtnGroup]).style('justify-content: end;')
                 ])
 
-                comments = sesh.query(Thread).filter_by(parent_id=thread.id).order_by(Thread.id.desc()).all()
+                # TODO change main file to sort comments by ascending order
+                comments = sesh.query(Thread).filter_by(parent_id=thread.id).order_by(Thread.id.asc()).all()
                 # print(len(comments))
                 if len(comments) != 0:  # show if there are comments in the thread
                     put_html('<p class="h5 fw-bolder">Comments</p>')
