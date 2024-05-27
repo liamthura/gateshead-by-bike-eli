5aafb223b53d19b0c9ae7c8103a3c0bf179bf190
khantthura <42053354+khantthura@users.noreply.github.com>
2024-05-27 06:42:42 +0100

    changes

--- main.py
+++ main.py
@@ -981,7 +981,7 @@ def own_forum_feeds():
             {'label': 'Create a new thread', 'value': 'create_thread', 'color': 'success'},
             {'label': 'All threads', 'value': 'view_all_threads', 'color': 'info'}
         ], onclick=[create_thread, forum_feeds]).style('float:right; margin-top: 12px;')
-    put_html('<h2>Community Forum</h2>')
+    put_html('<h2>My Forum Threads</h2>')
 
     get_threads(valid_user.id)
 
@@ -1046,7 +1046,8 @@ def get_threads(user_id=None):
                     put_column([threadBtnGroup]).style('justify-content: end;')
                 ])
 
-                comments = sesh.query(Thread).filter_by(parent_id=thread.id).order_by(Thread.id.desc()).all()
+                # TODO change main file to sort comments by ascending order
+                comments = sesh.query(Thread).filter_by(parent_id=thread.id).order_by(Thread.id.asc()).all()
                 # print(len(comments))
                 if len(comments) != 0:  # show if there are comments in the thread
                     put_html('<p class="h5 fw-bolder">Comments</p>')
@@ -2073,6 +2074,7 @@ def police_create_notification(pre_data=None):
         else:
             toast('Notification posted successfully', color='success')
         finally:
+            clear()
             notification_feeds()
 
     global valid_user
